b2948edfbf191bd10f0177fd33c49d8ee327dd94
Erin Sullivan <kittylily@gmail.com>
2016-11-01 11:56:35 -0700

    ⛓ Change brackets

--- donut-shader.fs.glsl
+++ donut-shader.fs.glsl
@@ -17,8 +17,7 @@ uniform float     iSampleRate;           // sound sample rate (i.e., 44100)
 // to orbitate the camera around the origin of the world (0,0,0), where
 // the yellow sphere is.
 //------------------------------------------------------------------------
-void doCamera(out vec3 camPos, out vec3 camTar, in float time, in float mouseX)
-{
+void doCamera(out vec3 camPos, out vec3 camTar, in float time, in float mouseX) {
   float an = 0.3 * iGlobalTime + 10.0 * mouseX;
   camPos = vec3(3.5 * sin(an), 1.0, 3.5 * cos(an));
   camTar = vec3(0.0, 0.0, 0.0);
@@ -29,8 +28,7 @@ void doCamera(out vec3 camPos, out vec3 camTar, in float time, in float mouseX)
 //
 // The background color. In this case it's just a black color.
 //------------------------------------------------------------------------
-vec3 doBackground(void)
-{
+vec3 doBackground(void) {
   return vec3(0.0, 0.0, 0.0);
 }
 
@@ -40,8 +38,7 @@ vec3 doBackground(void)
 // Defines the shapes (a sphere in this case) through a distance field, in
 // this case it's a torus.
 //------------------------------------------------------------------------
-float doModel(vec3 p)
-{
+float doModel(vec3 p) {
   vec2 t = vec2(1.0, 0.5);
   vec2 q = vec2(length(p.xz) - t.x, p.y);
   return length(q) - t.y;
@@ -54,8 +51,7 @@ float doModel(vec3 p)
 // at every point based on its position and normal. In this case, it simply
 // returns a constant pink color.
 //------------------------------------------------------------------------
-vec3 doMaterial(in vec3 pos, in vec3 nor)
-{
+vec3 doMaterial(in vec3 pos, in vec3 nor) {
   return vec3(0.48, 0.031, 0.20);
 }
 
@@ -64,8 +60,7 @@ vec3 doMaterial(in vec3 pos, in vec3 nor)
 //------------------------------------------------------------------------
 float calcSoftshadow(in vec3 ro, in vec3 rd);
 
-vec3 doLighting(in vec3 pos, in vec3 nor, in vec3 rd, in float dis, in vec3 mal)
-{
+vec3 doLighting(in vec3 pos, in vec3 nor, in vec3 rd, in float dis, in vec3 mal) {
   vec3 lin = vec3(0.0);
 
   // key light
@@ -89,17 +84,17 @@ vec3 doLighting(in vec3 pos, in vec3 nor, in vec3 rd, in float dis, in vec3 mal)
   return col;
 }
 
-float calcIntersection(in vec3 ro, in vec3 rd)
-{
+float calcIntersection(in vec3 ro, in vec3 rd) {
   const float maxd = 20.0;          // max trace distance
   const float precis = 0.001;       // precision of the intersection
   float h = precis * 2.0;
   float t = 0.0;
   float res = -1.0;
 
-  for(int i = 0; i < 90; i++)       // max number of raymarching iterations is 90
-  {
-    if(h < precis || t > maxd) break;
+  // max number of raymarching iterations is 90
+  for(int i = 0; i < 90; i++) {
+    if(h < precis || t > maxd)
+      break;
     h = doModel(ro + rd * t);
     t += h;
   }
@@ -108,8 +103,7 @@ float calcIntersection(in vec3 ro, in vec3 rd)
   return res;
 }
 
-vec3 calcNormal(in vec3 pos)
-{
+vec3 calcNormal(in vec3 pos) {
   const float eps = 0.002;          // precision of the normal computation
 
   const vec3 v1 = vec3(1.0, -1.0, -1.0);
@@ -123,13 +117,13 @@ vec3 calcNormal(in vec3 pos)
                    v4 * doModel(pos + v4 * eps));
 }
 
-float calcSoftshadow(in vec3 ro, in vec3 rd)
-{
+float calcSoftshadow(in vec3 ro, in vec3 rd) {
   float res = 1.0;
   float t = 0.0005;               // selfintersection avoidance distance
   float h = 1.0;
-  for(int i = 0; i < 40; i++)     // 40 is the max numnber of raymarching steps
-  {
+
+  // 40 is the max numnber of raymarching steps
+  for(int i = 0; i < 40; i++) {
     h = doModel(ro + rd * t);
     res = min(res, 64.0 * h / t); // 64 is the hardness of the shadows
     t += clamp(h, 0.02, 2.0);     // limit the max and min stepping distances
@@ -137,16 +131,14 @@ float calcSoftshadow(in vec3 ro, in vec3 rd)
   return clamp(res, 0.0, 1.0);
 }
 
-mat3 calcLookAtMatrix(in vec3 ro, in vec3 ta, in float roll)
-{
+mat3 calcLookAtMatrix(in vec3 ro, in vec3 ta, in float roll) {
   vec3 ww = normalize(ta - ro);
   vec3 uu = normalize(cross(ww, vec3(sin(roll), cos(roll), 0.0)));
   vec3 vv = normalize(cross(uu, ww));
   return mat3(uu, vv, ww);
 }
 
-void mainImage(out vec4 fragColor, in vec2 fragCoord)
-{
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
   vec2 p = (-iResolution.xy + 2.0 * fragCoord.xy) / iResolution.y;
   vec2 m = iMouse.xy / iResolution.xy;
 
@@ -172,8 +164,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
 
   // raymarch
   float t = calcIntersection(ro, rd);
-  if(t > -0.5)
-  {
+  if(t > -0.5) {
     // geometry
     vec3 pos = ro + t * rd;
     vec3 nor = calcNormal(pos);
